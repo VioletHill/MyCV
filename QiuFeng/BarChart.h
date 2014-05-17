7ef310921b3a5503f7b4d1139de9a6c60a24887e
vinago <vinago@yeah.net>
2014-05-17 19:30:21 +0800

    add image
    and fix BarChart.h for test

--- QiuFeng/BarChart.h
+++ QiuFeng/BarChart.h
@@ -1,10 +1,11 @@
-//
+﻿//
 //  BarChart.h
 //  QiuFeng
 //
 //  Created by 邱峰 on 4/7/14.
 //  Copyright (c) 2014 TongjiUniversity. All rights reserved.
 //
+//for test  by vinago
 
 #import <UIKit/UIKit.h>
 
